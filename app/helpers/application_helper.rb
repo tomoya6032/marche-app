2a58e585d5248cf535ff6c6ab3c82715865eb959
篠原智哉 <tomoya6032@yahoo.co.jp>
2026-09-15 13:28:21 +0900

    ショップの詳細ページで画像が表示されない不具合を修正中 それとOGP画像の設置できるようにした

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,37 @@
 module ApplicationHelper
+  def default_og_image_url
+    absolute_url(asset_path("marchelogo2.png"))
+  end
+
+  def shop_og_image_url(resource)
+    attachment = if resource.respond_to?(:images) && resource.images.attached?
+      resource.images.first
+    elsif resource.respond_to?(:top_image) && resource.top_image.attached?
+      resource.top_image
+    end
+
+    return default_og_image_url unless attachment
+
+    absolute_url(rails_storage_proxy_path(attachment))
+  rescue StandardError
+    default_og_image_url
+  end
+
+  def shop_og_description(resource)
+    resource.try(:description).presence || "つながるマルシェのショップ詳細ページです。"
+  end
+
+  def absolute_url(path_or_url)
+    value = path_or_url.to_s
+    return value if value.start_with?("http://", "https://")
+
+    base = request_base_url
+    return value if base.blank?
+
+    path = value.start_with?("/") ? value : "/#{value}"
+    "#{base}#{path}"
+  end
+
   def normalize_breadcrumb_path(path)
     return "#" if path.blank?
 
@@ -26,6 +59,21 @@ module ApplicationHelper
           end
         end.join.html_safe
       end
+    end
+  end
+
+  private
+
+  def request_base_url
+    if respond_to?(:request) && request.present?
+      request.base_url
+    else
+      defaults = Rails.application.config.action_controller.default_url_options || {}
+      host = defaults[:host].presence || ENV["APP_HOST"].presence
+      return nil if host.blank?
+
+      protocol = defaults[:protocol].presence || "https"
+      "#{protocol}://#{host}"
     end
   end
 end
